60b5978737d826534c5fe15654b593457640427d
jacky <jacky@metallurgietech.com>
2022-09-30 08:11:23 +0800

    完成基本動畫迴圈

---
+++ src/main.js
@@ -0,0 +1,87 @@
+//load事件時，畫出畫布
+window.addEventListener('load', function () {
+    //canvas setup 
+    var canvas = this.document.getElementById('canvas1');
+    var ctx = canvas.getContext('2d'); //Drawing Context(built in object that allow us to deal with canvas)
+    canvas.width = 1500;
+    canvas.height = 500;
+    var InputHandler = /** @class */ (function () {
+        function InputHandler() {
+        }
+        return InputHandler;
+    }());
+    var Projectile = /** @class */ (function () {
+        function Projectile() {
+        }
+        return Projectile;
+    }());
+    var Particle = /** @class */ (function () {
+        function Particle() {
+        }
+        return Particle;
+    }());
+    var Player = /** @class */ (function () {
+        function Player(game) {
+            this.game = game;
+            this.location = { x: 0, y: 0 };
+            this.speed = { x: 0, y: 0 };
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.location.x = 20;
+            this.location.y = 100;
+            this.speed.y = 1;
+            this.speed.x = 0;
+        }
+        Player.prototype.update = function () {
+            this.location.y += this.speed.y;
+        };
+        Player.prototype.draw = function (context) {
+            context.fillRect(this.location.x, this.location.y, this.width, this.height);
+        };
+        return Player;
+    }());
+    var Enemy = /** @class */ (function () {
+        function Enemy() {
+        }
+        return Enemy;
+    }());
+    var Layer = /** @class */ (function () {
+        function Layer() {
+        }
+        return Layer;
+    }());
+    var Background = /** @class */ (function () {
+        function Background() {
+        }
+        return Background;
+    }());
+    var UI = /** @class */ (function () {
+        function UI() {
+        }
+        return UI;
+    }());
+    var Game = /** @class */ (function () {
+        function Game(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            console.log(this.player);
+        }
+        Game.prototype.update = function () {
+            this.player.update();
+        };
+        Game.prototype.draw = function (context) {
+            this.player.draw(context);
+        };
+        return Game;
+    }());
+    var mainGame = new Game(canvas.width, canvas.height);
+    function animate() {
+        ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
+        mainGame.update();
+        mainGame.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
--- src/main.ts
+++ src/main.ts
@@ -22,19 +22,19 @@ window.addEventListener('load',function(){
         //初始化傳入一個Game物件，以和main game產生連結，取得資訊、變更屬性
         private width:number
         private height:number
-        private location:Coordinate
-        private speed:Speed
+        private location:Coordinate = {x:0,y:0}
+        private speed:Speed = {x:0,y:0}
         constructor(private game:Game) {
             this.game = game
             this.width = 120
             this.height = 190
             this.location.x = 20
             this.location.y = 100
-            this.speed.y = 0
+            this.speed.y = 1
             this.speed.x = 0
         }
         update(){
-            this.speed.y += this.speed.y
+            this.location.y += this.speed.y
         }
         draw(context:CanvasRenderingContext2D){
             context.fillRect(this.location.x,this.location.y,this.width,this.height)
@@ -49,13 +49,10 @@ window.addEventListener('load',function(){
 
     }
     class Game {
-        private width:number
-        private height:number
         private player:Player
-        constructor(width,height){
-            this.width = width
-            this.height = height
+        constructor(private width, private height){
             this.player = new Player(this)
+            console.log(this.player)
         }
         update(){
             this.player.update()
@@ -65,4 +62,11 @@ window.addEventListener('load',function(){
         }
     }
     const mainGame = new Game(canvas.width,canvas.height)
+    function animate() {
+        ctx?.clearRect(0,0,canvas.width,canvas.height)
+        mainGame.update()
+        mainGame.draw(ctx)
+        requestAnimationFrame(animate)
+    }
+    animate()
 })
